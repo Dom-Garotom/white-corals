465b841b015a14bc0bcfaf2eacd90df90143de74
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-30 14:05:10 -0300

    feat: criando componente para registrar os likes dos artigos

---
+++ src/components/atomos/likeStatus/index.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components"
+import { FaHeartCirclePlus } from "react-icons/fa6";
+import { useState } from "react";
+
+const Wrapper = styled.div`
+    width: 120px;
+    display : flex; 
+    font-weight: 700;
+    font-size: 1.2rem;
+    padding: 5px;
+    gap: 5px;
+    cursor:pointer;
+    border-radius: 4px;
+
+    &:hover{
+        background-color: #ece7e7;
+    }
+`
+
+export default function LikeStatus() {
+    const [numLikes , setNumLikes] = useState(120);
+    const [isSelect , setIsSelect] = useState(false);
+    const [clickAgain , setClickAgain] = useState(0);
+
+
+    const handleClick = () =>{
+        if (clickAgain != 0 && isSelect){
+            setNumLikes(numLikes - 1);
+            setClickAgain(0);
+            setIsSelect(!isSelect);
+            return;
+        }
+
+        if (!isSelect){
+            setNumLikes(numLikes + 1);
+            setIsSelect(!isSelect);
+            setClickAgain(clickAgain + 1);
+            return;
+        }
+    }
+
+    return (
+        <Wrapper  onClick={handleClick}>
+            <FaHeartCirclePlus />
+            <p>{numLikes} likes</p>
+        </Wrapper>
+    )
+}
