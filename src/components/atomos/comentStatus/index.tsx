c261dfc12d85f6734185ed99bc147050cc0ceca5
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-30 14:14:00 -0300

    feat: criando componente para registrar o numero de comentarios  dos artigos

--- src/components/atomos/comentStatus/index.tsx
+++ src/components/atomos/comentStatus/index.tsx
@@ -1,37 +1,38 @@
 import styled from "styled-components"
-import { FaHeartCirclePlus } from "react-icons/fa6";
+import { FaComments } from "react-icons/fa";
 import { useState } from "react";
 
 const Wrapper = styled.button`
-    display : flex; 
+    display : flex ; 
     font-weight: 700;
     font-size: 1.2rem;
     padding: 7px;
     gap: 5px;
     cursor:pointer;
     border-radius: 4px;
+    background-color: transparent;
 
     &:hover{
         background-color: #ece7e7;
     }
 `
 
-export default function LikeStatus() {
-    const [numLikes , setNumLikes] = useState(120);
+export default function ComentStatus() {
+    const [numComments , setnumComments] = useState(120);
     const [isSelect , setIsSelect] = useState(false);
     const [clickAgain , setClickAgain] = useState(0);
 
 
     const handleClick = () =>{
         if (clickAgain != 0 && isSelect){
-            setNumLikes(numLikes - 1);
+            setnumComments(numComments - 1);
             setClickAgain(0);
             setIsSelect(!isSelect);
             return;
         }
 
         if (!isSelect){
-            setNumLikes(numLikes + 1);
+            setnumComments(numComments + 1);
             setIsSelect(!isSelect);
             setClickAgain(clickAgain + 1);
             return;
@@ -40,8 +41,8 @@ export default function LikeStatus() {
 
     return (
         <Wrapper  onClick={handleClick}>
-            <FaHeartCirclePlus />
-            <p>{numLikes} likes</p>
+            <FaComments/>
+            <p>{numComments} comments</p>
         </Wrapper>
     )
 }
