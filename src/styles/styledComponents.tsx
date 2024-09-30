7bfc1cb597e5261f2e1957660659869f3a6a0b34
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-30 12:26:42 -0300

    feat: criando o desing system das tags dos articles

--- src/styles/styledComponents.tsx
+++ src/styles/styledComponents.tsx
@@ -8,12 +8,31 @@ type propsTitle = {
 
 export const TitleMain = styled.h1<propsTitle>`
     font-weight: ${ (prop) => prop.weight ? prop.weight : 700};
-    font-size: ${ (prop) => prop.size ? prop.size : '3rem'};;
-    color: ${ (prop) => prop.color ? prop.color : "#000"};;
+    font-size: ${ (prop) => prop.size ? prop.size : '3rem'};
+    color: ${ (prop) => prop.color ? prop.color : "#000"};
 `
 
 export const TitleDefault = styled.h2<propsTitle>`
     font-weight: ${ (prop) => prop.weight ? prop.weight : 700};
-    font-size: ${ (prop) => prop.size ? prop.size : '2rem'};;
-    color: ${ (prop) => prop.color ? prop.color : "#000"};;
+    font-size: ${ (prop) => prop.size ? prop.size : '2rem'};
+    color: ${ (prop) => prop.color ? prop.color : "#000"};
+`
+
+
+type propsArticles = {
+    color ?: string; 
+}
+
+export const TagsArticles = styled.p<propsArticles>`
+    font-weight: 700;
+    font-size: 1rem;
+    color:#878A92;
+
+    &::before{
+        content: "#";
+        color: ${ prop => prop.color ? prop.color : "#878A92"} ;
+        font-size: 1.1rem;
+        font-weight: 700;
+        margin-right: 3px ;
+    }
 `
