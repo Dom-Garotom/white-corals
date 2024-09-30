585baaf5202519673bf75428e364eb2996261a77
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-09-30 14:27:18 -0300

    feat: criando componente para registrar os artigos que o user quer deixar salvo

---
+++ src/components/atomos/saveStatus/index.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components"
+import { FaBookmark } from "react-icons/fa";
+import { FaRegBookmark } from "react-icons/fa6";
+import { useState } from "react";
+
+const Wrapper = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    font-size: 1.5rem;
+    padding: 7px;
+    gap: 5px;
+    cursor:pointer;
+    border-radius: 4px;
+    background-color: transparent;
+    
+    &:hover{
+        color: #5353ff;
+        background-color: #7070ff26;
+    }
+
+`
+
+export default function SaveStatus() {
+    const [isSelect, setIsSelect] = useState(false);
+
+
+    const handleClick = () => {
+        setIsSelect(!isSelect);
+    }
+
+    return (
+        <Wrapper onClick={handleClick}>
+            {isSelect ? (
+                <FaRegBookmark />
+            ) : (
+                <FaBookmark />
+            )}
+        </Wrapper>
+    )
+}
