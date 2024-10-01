30488b3f98d0d3421d36e2df39fcf9c71d0537e5
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-01 07:21:16 -0300

    feat: criando o header padrão da aplicação.

---
+++ src/components/organismos/header/headerStyled.tsx
@@ -0,0 +1,119 @@
+import styled from "styled-components"
+
+export const WrapperHeader = styled.header`
+    min-block-size: 64px;
+`
+
+export const Wrapper = styled.div`
+    display: flex;
+    gap: 20px;
+
+    span{
+        font-weight: 600;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        cursor: pointer;
+
+        &::before{
+            content: "";
+            height: 25px;
+            width: 2px;
+            background-color: #121212;
+        }
+
+        &:hover{
+            color: #3B6AE1;
+        }
+    }
+
+
+    a{
+        text-decoration: none;
+        font-weight: 600;
+        color: #121212;
+
+        &:hover{
+            color: #3B6AE1;
+        }
+    }
+
+    div{
+        display: flex;
+        gap: 20px;
+        align-items: center;
+    }
+
+    #NameUser{
+        display: flex;
+        gap: 10px;
+    }
+
+    @media screen and (max-width : 800px) {
+        gap: 5px;
+
+        #NameUser{
+            margin-right: 5px;
+        }
+
+        span::before{
+            content: none;
+        }
+
+        span::after{
+            content: "";
+            height: 25px;
+            width: 2px;
+            background-color: #121212;
+        }
+    }
+
+
+
+`
+
+export const WrapperContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 40px;
+    background-color: #FFF;
+
+    @media screen and (max-width: 500px) {
+        padding: 0px 0px;        
+    }
+`
+
+type MenuWrapper = {
+    block : boolean;
+}
+export const WrapperMenu = styled.div<MenuWrapper>`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #F5F5F5;
+    gap: 30px;
+    padding: 20px 0px;
+
+    a{
+        text-decoration: none;
+        font-weight: 600;
+        color: #121212;
+
+        &:hover{
+            color: #3B6AE1;
+        }
+    }
+
+    pointer-events: ${(props) => (props.block ? 'auto' : 'none')};
+    display: ${(props) => (props.block ? 'flex' : 'none')};
+    opacity: ${(props) => (props.block ? '1' : '0')};
+    transform: ${(props) => (props.block ? 'translateY(0)' : 'translateY(1rem)')};
+    transition: opacity 0.20s ease, transform 0.60s ease;
+
+`
+
+
---
+++ src/components/organismos/header/index.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image"
+import Link from "next/link"
+import { IoIosArrowDown } from "react-icons/io";
+import LogoHeader from "@/public/logo-header.png"
+import { AiOutlineMenu } from "react-icons/ai";
+import { ButtonMenuHeader } from "@/styles/styledComponents";
+import { useEffect, useRef, useState } from "react";
+import { WrapperHeader ,Wrapper , WrapperContainer , WrapperMenu } from "./headerStyled";
+
+
+export default function Header() {
+    const header = useRef<HTMLDivElement>(null);
+    const [widthScreen, setWidthScreen] = useState(0);
+    const [isVisble, setIsVisible] = useState(false);
+    const [menuIsVisible, setMenuIsVisble] = useState(false)
+
+    const togglVisibilityMenu = () =>{
+        setMenuIsVisble(!menuIsVisible)
+    }
+
+    useEffect(() => {
+        const handleWidthScreen = () => {
+            if (header.current) {
+                setWidthScreen(header.current.offsetWidth);
+            }
+        }
+
+        handleWidthScreen();
+
+        window.addEventListener("resize", handleWidthScreen)
+
+        if (widthScreen < 800) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false)
+        }
+
+        return () => {
+            window.removeEventListener("resize", handleWidthScreen);
+        }
+    })
+
+
+    return (
+        <WrapperHeader ref={header}>
+            <WrapperContainer>
+                <Link href={''}>
+                    <Image src={LogoHeader} alt="Logo da white corals" />
+                </Link>
+
+                <Wrapper>
+                    {!isVisble &&
+                        <div>
+                            <Link href={''}>Home</Link>
+                            <Link href={''}>Meus Artigos</Link>
+                            <Link href={''}>Publicar artigo</Link>
+                        </div>
+                    }
+
+                    <span>
+                        olá,
+                        <Link href={''} id="NameUser">
+                            <b>Karina</b>
+                            <IoIosArrowDown />
+                        </Link>
+                    </span>
+
+                    <ButtonMenuHeader block={isVisble} onClick={() => togglVisibilityMenu()}>
+                        <AiOutlineMenu />
+                    </ButtonMenuHeader>
+
+                </Wrapper>
+            </WrapperContainer>
+            <WrapperMenu block={menuIsVisible}>
+                <Link href={''}>Home</Link>
+                <Link href={''}>Meus Artigos</Link>
+                <Link href={''}>Publicar artigo</Link>
+            </WrapperMenu>
+
+        </WrapperHeader>
+    )
+}
