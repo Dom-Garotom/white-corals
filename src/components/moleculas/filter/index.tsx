496744cd546b529783e3ed660f6d038778d97604
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-01 15:30:26 -0300

    feat: implementando acessibilidade do title ao filter

--- src/components/moleculas/filter/index.tsx
+++ src/components/moleculas/filter/index.tsx
@@ -11,14 +11,17 @@ import { FaPlus } from "react-icons/fa";
 import StatusDefault from "@/components/atomos/statusDefault";
 
 const Wrapper = styled.div`
+  width: 70px;
+  height: 280px;
   background-color: #fff;
   padding: 10px;
   border-radius: 10px;
   box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
 
-  @media screen and (max-width: 760px) {
+  @media screen and (max-width: 1000px) {
     display: flex;
     width: 100%;
+    height: 70px;
     justify-content: space-around;
   }
 `
@@ -30,26 +33,31 @@ export default function FilterArticles() {
       <StatusDefault
         iconDefault={<FaStar />}
         iconFull={<CiStar />}
+        title="Mais populares"
       />
 
       <StatusDefault
         iconDefault={<MdGroups3 />}
         iconFull={<MdOutlineGroups3 />}
+        title="Mais relevantes"
       />
 
       <StatusDefault
         iconDefault={<MdTimer />}
         iconFull={<RiTimerLine />}
+        title="Recentes"
       />
 
       <StatusDefault
         iconDefault={<FaBookmark />}
         iconFull={<FaRegBookmark />}
+        title="Salvos"
       />
 
       <StatusDefault
         iconDefault={<FaPlus />}
         iconFull={<FaPlus />}
+        title="Criar um post"
       />
 
     </Wrapper>
