49c51778c2d18d8590bef6c5eb2b3a5a393d93eb
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-02 15:20:04 -0300

    feat:criando a página inicial do sistema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,49 @@
 "use client"
+import styled from "styled-components";
+import Footer from "@/components/moleculas/footer";
+import BackGround from "@/public/backGround-login.png"
+import InfoEvent from "@/components/moleculas/infoEvent";
+import Login from "@/components/organismos/Login";
 
-import { TitleMain } from "@/styles/styledComponents";
+
+const WrapperMain = styled.main`
+  background-image: url(${BackGround.src});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  min-block-size: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+  padding: 20px;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+  }
 
 
-export default function Home() {
+
+`
+
+const Wrapper = styled.body`
+  footer{
+    margin: 0px;
+  }
+`
+
+export default function LoginPage() {
   return (
-    <div >
-      <TitleMain>Alien Defender Game</TitleMain>
-    </div>
+    <Wrapper>
+      <WrapperMain>
+        <InfoEvent/>
+        <Login/>
+
+      </WrapperMain>
+      <Footer/>
+    </Wrapper>
   );
+
+
 }
