88134ad16885fbe7a9b9134811de44449d8ce0b1
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-03 09:57:48 -0300

    feat: deixando o input default abel a usar o react hook forms

--- src/components/moleculas/inputDefault/index.tsx
+++ src/components/moleculas/inputDefault/index.tsx
@@ -1,33 +1,44 @@
 import { Wrapper , WrapperContainer , ErrorP } from "./styled-Input";
+import { UseFormRegisterReturn } from "react-hook-form";
 import { useState } from "react";
 
-{/* Ele pega as props padroes do input  */ }
+
 
 interface InputDefaultProps extends React.InputHTMLAttributes<HTMLElement> {
   label: string;
   icon: React.ReactNode;
   InputError?: boolean;
-  mensageError?: string
+  mensageError?: string;
+  register?: UseFormRegisterReturn;
 }
 
 
-export default function InputDefault({ label, icon, InputError, mensageError, ...props }: InputDefaultProps) {
+export default function InputDefault({ label, icon, InputError, mensageError , register ,...props }: InputDefaultProps) {
   const [isFocus, setIsFocus] = useState(false);
 
-  // verificar se o input está vázio ou não para manter o label no topo 
 
   return (
     <WrapperContainer>
 
       <Wrapper $InputError={InputError}>
         {icon}
-        <input {...props}
-          onFocus={() => setIsFocus(true)}
 
-          required />
-        <label htmlFor={props.id} style={{ transform: isFocus || props.value ? 'translateY(-30px)' : 'translateY(0px)' }}>
+        <input 
+          onFocus={() => setIsFocus(true)}
+          {...register}
+          {...props}
+          required 
+          />
+
+        <label 
+          htmlFor={props.id} 
+          style={{ transform: isFocus || props.value ? 'translateY(-30px)' : 'translateY(0px)' }}
+          >
+
           {label}
+
         </label>
+
       </Wrapper>
 
       {InputError && 
