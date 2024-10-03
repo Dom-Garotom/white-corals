6f8779aff67297ce1b15de4a0357395e181f9b63
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-03 05:45:25 -0300

    feat: criando a página de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+import styled from "styled-components";
+import Footer from "@/components/moleculas/footer";
+import BackGround from "@/public/backGround-login.png"
+import InfoEvent from "@/components/moleculas/infoEvent";
+import Login from "@/components/organismos/login";
+
+
+const WrapperMain = styled.main`
+  background-image: url(${BackGround.src});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  min-block-size: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+  padding: 20px;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+  }
+
+
+
+`
+
+const Wrapper = styled.body`
+  footer{
+    margin: 0px;
+  }
+`
+
+export default function LoginPage() {
+  return (
+    <Wrapper>
+      <WrapperMain>
+        <InfoEvent/>
+        <Login/>
+
+      </WrapperMain>
+      <Footer/>
+    </Wrapper>
+  );
+
+
+}
