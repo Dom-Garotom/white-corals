0c29c8c6035d9be81f4def7468902945d8eec0f8
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-05 12:01:45 -0300

    feat: dando a capacidade do componente armazernar e contabilizar likes

--- src/components/atomos/statusDefault/index.tsx
+++ src/components/atomos/statusDefault/index.tsx
@@ -1,30 +1,58 @@
 import { useState } from "react";
 import { ButtonFilter } from "@/styles/styledComponents";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px 0px;
+`
 
 type prop = {
-    iconDefault : React.ReactNode,
-    iconFull :  React.ReactNode;
-    handleFunction ?: () => void;
-    title ?: string;
+    iconDefault: React.ReactNode,
+    iconFull: React.ReactNode;
+    handleFunction?: () => void;
+    title?: string;
+    number?: number;
 }
 
-export default function Statusdefault({ iconDefault , iconFull , handleFunction , title } : prop) {
-    const [isSelect, setIsSelect] = useState(false);
+export default function Statusdefault({ iconDefault, iconFull, handleFunction, title, number }: prop) {
+    const [numLikes , setNumLikes] = useState(number ? number : 0);
+    const [isSelect , setIsSelect] = useState(false);
+    const [clickAgain , setClickAgain] = useState(0);
 
 
-    const handleClick = () => {
-        setIsSelect(!isSelect);
+    const handleClick = () =>{
+        if (clickAgain != 0 && isSelect){
+            setNumLikes(numLikes - 1);
+            setClickAgain(0);
+            setIsSelect(!isSelect);
+            return;
+        }
+
+        if (!isSelect){
+            setNumLikes(numLikes + 1);
+            setIsSelect(!isSelect);
+            setClickAgain(clickAgain + 1);
+            return;
+        }
     }
 
+
+
     return (
-        <ButtonFilter onClick={handleFunction}>
-            <span title={title} onClick={handleClick}>
-            {isSelect ? (
-                iconDefault
-            ) : (
-                iconFull
-            )}
-            </span>
-        </ButtonFilter>
+        <Wrapper>
+            <ButtonFilter onClick={handleFunction}>
+                <span title={title} onClick={handleClick}>
+                    {isSelect ? (
+                        iconDefault
+                    ) : (
+                        iconFull
+                    )}
+                </span>
+            </ButtonFilter>
+            {number && numLikes}
+        </Wrapper>
     )
 }
