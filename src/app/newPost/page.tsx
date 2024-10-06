ab4dc6a94491abab375499c6fddd8c27870665a8
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-06 08:49:43 -0300

    refactor: mudando a forma como as tags eram coletadas e armazenadas no objeto que ia para o banco de dados do localhost como também teve a adição de renderizar as tags em tela para o usuario

--- src/app/newPost/page.tsx
+++ src/app/newPost/page.tsx
@@ -3,13 +3,14 @@
 import { WrapperBody, WrapperImageContainer, WrapperMain } from "./styled";
 import Header from "@/components/organismos/header";
 import InputDefault from "@/components/moleculas/inputDefault";
-import { TitleDefault } from "@/styles/styledComponents";
+import { TagsArticles, TitleDefault } from "@/styles/styledComponents";
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
 import styled from "styled-components"
 import { z } from "zod";
-import { Toaster , toast } from "sonner";
+import { Toaster, toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 const Wrapper = styled.form`
@@ -27,6 +28,11 @@ const Wrapper = styled.form`
 
   label{
     font-size: 1.5rem;
+  }
+
+  .tags{
+    display: flex;
+    flex-wrap: wrap;
   }
 
   textarea{
@@ -94,53 +100,95 @@ const Wrapper = styled.form`
 
 const ArticleSchema = z.object({
   id: z.string(),
-  title: z.string(),
-  article: z.string(),
-  tags: z.string().transform((tags) =>
-    tags.split(",").map(tag => ({
-      color: "#ffcc00", 
-      content: tag.trim()
-    }))
-  ),
+  title: z.string().min(4, "O título é obrigatório"),
+  article: z.string().min(20, "O artigo é obrigatório"),
+  tags: z.array(z.object({
+    content: z.string(),
+    color: z.string()
+  })),
   likes: z.number(),
   comentarios: z.number(),
   saved: z.boolean(),
-  image: z.string(),
-})
+  image: z.string().url("A URL da imagem deve ser válida"),
+});
 
 type ArticleSchema = z.infer<typeof ArticleSchema>;
 
 export default function NewPost() {
   const router = useRouter();
-  const { register, handleSubmit } = useForm<ArticleSchema>();
+  const { register, handleSubmit, formState: { errors } } = useForm<ArticleSchema>();
+  const [ containerDataObject , setContainerDataObject] = useState<ArticleSchema>({
+    id: "",
+    title: "",
+    article: "",
+    tags: [],
+    likes: 0,
+    comentarios: 0,
+    saved: false,
+    image: "",
+  })
+
+  const [debounceTimeOut, setDebouncetimout] = useState<NodeJS.Timeout | null>(null);
+
+  const handleTag = (a: React.ChangeEvent<HTMLInputElement>) => {
+    const tag = a.currentTarget.value.trim();
+
+    if (debounceTimeOut) {
+      clearTimeout(debounceTimeOut);
+    }
+
+    setDebouncetimout(
+
+      setTimeout(() => {
+
+        if (tag && containerDataObject) {
+          setContainerDataObject(prev => ({
+            ...prev,
+            tags: [...prev.tags, { content: tag, color: getRandomHexColor() }],
+          }));
+
+        }
+
+        if (a.currentTarget) {
+          a.currentTarget.value = " ";
+        } 
+        
+      }, 3000)
+    )
+    
+  }
+
+  const getRandomHexColor = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${randomColor.padStart(6, '0')}`;
+  };
+  
+
 
   const handleInformation: SubmitHandler<ArticleSchema> = (data) => {
 
-    const dataObject = data;
-    dataObject.id = String(self.crypto.randomUUID());
-    dataObject.likes = 0 ;
-    dataObject.comentarios = 0;
-    dataObject.saved = false;
-    
-  
-    dataObject.tags = Array.isArray(dataObject.tags) ? dataObject.tags.map((tag, index) => ({
-      ...tag,
-      color: index % 2 === 0 ? "#ffcc00" : "#00ffcc", 
-    })) : [];
-
+    const dataObject : ArticleSchema = {
+      ...data,
+      id: String(self.crypto.randomUUID()),
+      likes: 0,
+      comentarios: 0,
+      saved: false,
+      tags: containerDataObject.tags,
+    }
 
     const getLocalStorage = JSON.parse(String(localStorage.getItem("article")));
-    let storage : ArticleSchema[] = []; 
+    let storage: ArticleSchema[] = [];
 
     storage = Array.isArray(getLocalStorage) ? getLocalStorage : [];
 
     storage.push(dataObject);
 
     localStorage.setItem("article", JSON.stringify(storage));
-    
+
     toast.success("Post criado com sucesso")
 
     router.push("/home");
+
   }
 
 
@@ -174,6 +222,7 @@ export default function NewPost() {
             register={register("image")}
             required
           />
+          {errors.image && <p>{errors.image.message}</p>}
 
 
 
@@ -187,6 +236,7 @@ export default function NewPost() {
             register={register("title")}
             required
           />
+          {errors.image && <p>{errors.image.message}</p>}
 
           <TitleDefault size="1.2rem">Adicionar tag</TitleDefault>
 
@@ -197,10 +247,21 @@ export default function NewPost() {
             type="text"
             borderColor="#000"
             register={register("tags")}
+            onChange={handleTag}
             required
           />
+          {errors.tags && <p>{errors.tags.message}</p>}
+          <p>Espere 3s antes de adicionar outra tag</p>
+
+          <div className="tags">
+            {containerDataObject.tags.map((tag, index) => (
+              <TagsArticles key={index} color={tag.color} >{tag.content}</TagsArticles>
+            ))}
+          </div>
+
 
           <textarea placeholder="Artigo aqui..."  {...register("article")} required />
+          {errors.article && <p>{errors.article.message}</p>}
 
           <p>
             Nosso Criador de Post é uma ferramenta exclusiva para escritores que permite a criação de artigos utilizando a
