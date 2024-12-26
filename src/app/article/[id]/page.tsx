73961c350b167266af01b73492d71ae4cdc55254
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-12-26 15:09:33 -0300

    refactor: refactor individual article page

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,128 +1,64 @@
 "use client"
 
-import styled from "styled-components"
 import Footer from "@/components/moleculas/footer"
 import Header from "@/components/organismos/header"
 import Article from "@/components/moleculas/article"
 import StatusBar from "@/components/moleculas/statusBar"
 import { useParams } from "next/navigation"
 import {  DataBase, dataBase, myDataBase } from "@/db"
-import { useEffect, useState } from "react"
-
-const Wrapper = styled.main`
-
-`
-
-const WrapperArticleContainer = styled.section`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 2fr minmax(350px, 800px) 2fr;
-  padding: 20px;
-
-  @media (max-width: 1000px) {
-    grid-template-columns: 1fr;
-  }
-  `
-
-const WrapperArticles = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  flex: 1 0 auto;
-
-  @media (max-width: 1000px) {
-    align-items: center;
-  }
-`
-
-const WrapperFilter = styled.div`
-  display: flex;
-  justify-content: end;
-  padding: 20px;
-
-  .container{
-    position: fixed;
-  }
-
-
-  @media screen and (max-width: 1000px) {
-    grid-column: 1 / -1;
-    justify-content: center;
-    padding: 10px;
-    
-    .container{
-      width: 95%;
-      position: fixed;
-      top: 30px;
-      top: 92%;
-      
-    }
-  
-  }
-
-`
+import {  useEffect, useState } from "react"
+import { Wrapper } from "./style"
 
 
 export default function ArticlesPage() {
   const { id } = useParams();
-  const [logsArticle, setLogsArticle] = useState<DataBase[]>([]);
-
-  // const getLocalStorage = JSON.parse(String(localStorage.getItem("article")));
-
-
-  // let dataLocalStorage = [];
-
-  // dataLocalStorage = Array.isArray(getLocalStorage) ? getLocalStorage : [];
-  
-  // dataBase.map( elemnt =>{
-  //   dataLocalStorage.push(elemnt);
-  // })
+  const [articleData, setArticleData ] = useState<DataBase>();
 
 
   useEffect(() => {
-    const db = dataBase;
+    const articlesDataBase = dataBase;
 
-    const myDb = myDataBase;
+    const myArticles  = myDataBase;
 
-    const bancoDeDados = [...myDb ,...db];
+    const currentArticlesDataBase : DataBase[] = [...articlesDataBase ,...myArticles];
 
-    setLogsArticle(bancoDeDados)
+    const article = currentArticlesDataBase.find( element => element.id === id );
 
-  }, [])
+    setArticleData(article);
+  } , [])
 
-  const dataArticle = logsArticle.find( element => element.id === id );
 
-  if (!dataArticle) {
-    return <p>Article not found</p>;
+  if (!articleData) {
+    return <h2>🌀 Loading...</h2>;;
   }
 
   return (
-    <Wrapper>
+    <Wrapper.root>
       <Header />
 
-      <WrapperArticleContainer>
+      <Wrapper.ArticleContainer>
 
-        <WrapperFilter>
+        <Wrapper.FilterBar>
           <div className="container">
-            <StatusBar likes={dataArticle.likes} comentarios={dataArticle.comentarios} />
+            <StatusBar likes={articleData.likes} comentarios={articleData.comentarios} />
           </div>
-        </WrapperFilter>
+        </Wrapper.FilterBar>
 
-        <WrapperArticles>
+        <Wrapper.Article>
 
           <Article
-            id={dataArticle.id}
-            image={dataArticle.image}
-            article={dataArticle.article}
-            title={dataArticle.title}
-            tags={dataArticle.tags}
+            id={articleData.id}
+            image={articleData.image}
+            article={articleData.article}
+            title={articleData.title}
+            tags={articleData.tags}
           />
 
-        </WrapperArticles>
+        </Wrapper.Article>
 
-      </WrapperArticleContainer>
+      </Wrapper.ArticleContainer>
 
       <Footer />
-    </Wrapper>
+    </Wrapper.root>
   )
 }
